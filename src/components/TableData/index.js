2d94b2bfe3ee16c6aa0be84e4ebbb05a651d3c42
Mac Mini <a049@ec2-16-16-16-107.eu-north-1.compute.amazonaws.com>
2022-05-31 14:48:26 +0700

    style: table data

--- src/components/TableData/index.js
+++ src/components/TableData/index.js
@@ -1,12 +1,42 @@
 import React from 'react';
-import DataTable from 'react-data-table-component';
+import DataTable, { createTheme } from 'react-data-table-component';
+import { customStyleTable } from './table.style';
 
 export default function TableData({ columns, data, isLoading }) {
   return (
     <div>
-      <DataTable columns={columns} data={data} direction="RTL" progressPending={isLoading} />
+      <DataTable
+        highlightOnHover
+        pointerOnHover
+        columns={columns}
+        data={data}
+        direction="RTL"
+        progressPending={isLoading}
+        theme="solarized"
+        customStyles={customStyleTable}
+      />
 
-      {/* <DataTable columns={columns} data={data} direction="LTR" progressPending={isLoading} /> */}
+      {/* <DataTable columns={columns} data={data} direction="LTR" progressPending={isLoading} theme="solarized" /> */}
     </div>
   );
 }
+
+createTheme('solarized', {
+  context: {
+    background: '#cb4b16',
+    text: '#FFFFFF',
+  },
+  divider: {
+    default: '#D8DCD6',
+  },
+  button: {
+    default: '#2aa198',
+    // hover: 'rgba(0,0,0,.08)',
+    hover: 'red',
+    focus: 'rgba(255,255,255,.12)',
+    disabled: 'rgba(255, 255, 255, .34)',
+  },
+  sortFocus: {
+    default: '#2aa198',
+  },
+});
---
+++ src/components/TableData/table.style.js
@@ -0,0 +1,16 @@
+export const customStyleTable = {
+  headCells: {
+    style: {
+      fontSize: 14,
+      backgroundColor: '#F5F3F5',
+    },
+  },
+  cells: {
+    style: {},
+  },
+  rows: {
+    highlightOnHoverStyle: {
+      backgroundColor: 'rgba(205,205,205,0.15)',
+    },
+  },
+};
