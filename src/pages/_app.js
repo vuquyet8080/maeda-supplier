a47594bd91deb6d0a7c7a8e85c47dd3aee126ff1
vũ quyết <vuquyet@ec2-16-16-16-118.eu-north-1.compute.amazonaws.com>
2022-06-07 15:50:15 +0700

    save setting language to local storage

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -4,13 +4,23 @@ import arabic from 'language/arabic.json';
 import english from 'language/english.json';
 import { SessionProvider } from 'next-auth/react';
 import { useRouter } from 'next/router';
+import { useEffect } from 'react';
 import { I18nextProvider, initReactI18next } from 'react-i18next';
 import { ToastContainer } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
 import 'styles/globals.scss';
 
 export default function App({ Component, pageProps: { session, ...pageProps } }) {
-  const { locale } = useRouter();
+  const { locale, push } = useRouter();
+
+  useEffect(() => {
+    const language = localStorage.getItem('language');
+    push(`/${language}`);
+  }, []);
+
+  useEffect(() => {
+    localStorage.setItem('language', locale);
+  }, [locale]);
 
   i18next.use(initReactI18next).init({
     interpolation: { escapeValue: false }, // React already does escaping
