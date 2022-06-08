019bce52553159d78392510d9af25a6d1b2db3d7
vũ quyết <vuquyet@ec2-16-16-16-127.eu-north-1.compute.amazonaws.com>
2022-06-08 16:21:43 +0700

    fix: show data driver detail

--- src/components/DriverProfile/DataProfile.js
+++ src/components/DriverProfile/DataProfile.js
@@ -3,42 +3,43 @@ import { getInfoDriverDetail } from 'actions/driver';
 import React, { useEffect, useState } from 'react';
 import { useTranslation } from 'react-i18next';
 
-const renderRowData = (name, value) => (
-  <div className="grid md:grid-cols-3 grid-cols-2 lg:w-[650px] md:w-[450px] w-full md:mb-4 mb-3 lg:mb-6  2xl:mb-8">
-    <div className="md:col-span-2 col-span-1 text-xs md:text-sm ">{name}:</div>
-    <div className="col-span-1 text-xs md:text-sm ">{value}</div>
-  </div>
-);
 export default function DataProfile({ idDriver }) {
   const [dataProfile, setDataProfile] = useState();
   const { t } = useTranslation();
+
+  const renderRowData = (name, value) => (
+    <div className="grid md:grid-cols-3 grid-cols-2 lg:w-[650px] md:w-[450px] w-full md:mb-4 mb-3 lg:mb-6  2xl:mb-8">
+      <div className="md:col-span-2 col-span-1 text-xs md:text-sm ">{name}</div>
+      <div className="col-span-1 text-xs md:text-sm ">{value}</div>
+    </div>
+  );
 
   const fetchDataDetailDriver = async () => {
     try {
       const response = await getInfoDriverDetail({ id: idDriver });
       const {
-        data: { name, company_name, car_number, phone, city, region },
+        data: { name, company_name, id_number, phone, city, region },
       } = response;
       setDataProfile([
-        { title: t('driverDetail.name'), value: name },
+        { title: 'name', value: name },
         {
-          title: t('driverDetail.companyName'),
+          title: 'companyName',
           value: company_name,
         },
         {
-          title: t('driverDetail.cartId'),
-          value: car_number,
+          title: 'cartId',
+          value: id_number,
         },
         {
-          title: t('driverDetail.phone'),
+          title: 'phone',
           value: phone,
         },
         {
-          title: t('driverDetail.city'),
+          title: 'city',
           value: city,
         },
         {
-          title: t('driverDetail.region'),
+          title: 'region',
           value: region,
         },
       ]);
@@ -53,7 +54,7 @@ export default function DataProfile({ idDriver }) {
   return (
     <div className="lg:pt-16 2xl:pt-12 xl:pt-10 md:pt-8 pt-6">
       {dataProfile?.map((item, index) => (
-        <div key={index}>{renderRowData(item.title, item.value)}</div>
+        <div key={index}>{renderRowData(t(`driverDetail.${item.title}`), item.value)}</div>
       ))}
     </div>
   );
