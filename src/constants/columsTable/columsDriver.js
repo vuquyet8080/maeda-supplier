d761216b4de8281372912e3a9abd27922514b3ff
vũ quyết <vuquyet@vus-MacBook-Pro.local>
2022-06-14 16:11:23 +0700

    style:  set width driver table

--- src/constants/columsTable/columsDriver.js
+++ src/constants/columsTable/columsDriver.js
@@ -18,40 +18,47 @@ export const useColumnsTableDriver = () => {
     {
       name: t('driver.name'),
       selector: (row) => row.name,
+      width: '250px',
     },
     {
       name: t('driver.id'),
       selector: (row) => row.id_number,
+      width: '200px',
     },
     {
       name: t('driver.status'),
       selector: (row) => renderStatus(row.isActive),
+      width: '200px',
     },
     {
       name: t('driver.rate'),
       selector: (row) => (row?.acceptanceRate ? formatNumber(row?.acceptanceRate) : '---'),
+      width: '150px',
     },
     {
       name: t('driver.trips'),
       selector: (row) => (isEmpty(row.trips) ? 0 : formatNumber({ number: row.trips[0].count })),
+      width: '150px',
     },
     {
       name: t('driver.earning'),
       selector: (row) => (row?.earnAmount ? formatNumber({ number: row?.earnAmount }) : 0),
+      width: '150px',
     },
     {
       name: t('driver.mainBalance'),
       selector: (row) =>
         isEmpty(row.balances) ? 0 : formatNumber({ number: row.balances[0].new_balance }),
+      width: '250px',
     },
     {
       name: t('driver.point'),
       selector: (row) => row.year,
+      width: '150px',
     },
     {
       name: t('driver.createAt'),
       selector: (row) => (row?.created_at ? formatDate(row?.created_at) : ''),
-      width: '200px',
     },
   ];
 };
